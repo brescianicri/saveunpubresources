904b5ef65ee2e14e9147b8fd16537cfdb5a6ffe7
brescianicri <brescianicristian@ymail.com>
2018-02-05 09:38:00 +0100

    Create SaveUnpubResources.php

---
+++ SaveUnpubResources.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * SaveUnpubResources beta 0.0.1
+ *
+ * DESCRIPTION
+ *
+ * This Plugin write Modx Resources with Unpublished date in external table call: resoruce_unpub
+ *
+ * OnDocFormSave Event
+ *
+ *
+ */
+
+
+$unpub_date = $resource->get('unpub_date');
+
+// eseguo lo script solo quando ho la data di spubblicazione
+if ($unpub_date !== 0){
+  $id = $resource->get('id');
+
+  //trovo id della risorsa e data di spubblicazione
+  $modx->log(modX::LOG_LEVEL_ERROR, '[SaveUnpub] id'.$id);
+  $modx->log(modX::LOG_LEVEL_ERROR, '[SaveUnpub] unpub_date'.$unpub_date);
+
+  //converto la data di spubblicazione in un timestamp
+  $unpub_date_format = strtotime($unpub_date) ;
+
+  //inserisco i dati nella tabella
+  $sql = 'INSERT INTO resoruce_unpub (resource_id, unpub_date) VALUES ('.$id.', '.$unpub_date_format.' )';
+  $stmt = $modx->prepare($sql);
+  $stmt->execute();
+
+  //mi scrivo un log con le righe inserite
+  $modx->log(modX::LOG_LEVEL_ERROR, $modx->lastInsertId());
+}
